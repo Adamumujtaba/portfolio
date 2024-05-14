0251e178d6fe4bc8b74554d49eed7506d01cce0e
MujtabaAdamu <mujtaba.adamu@flexisaf.com>
2024-05-14 14:56:43 +0100

    Add footer

---
+++ src/App.tsx
@@ -0,0 +1,282 @@
+import { useState } from 'react';
+import { CgClose } from 'react-icons/cg';
+import { FaBars, FaFacebook } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { LiaLinkedin } from 'react-icons/lia';
+import { GrGithub } from 'react-icons/gr';
+import { BsTwitterX } from 'react-icons/bs';
+import {
+  Button,
+  DesktopList,
+  DesktopListItem,
+  HeaderAbout,
+  Cont,
+  HeaderTitle,
+  LeftEmail,
+  Logo,
+  MenuBar,
+  MiniHeader,
+  MobileList,
+  Nav,
+  RightMenuIcon,
+  Section,
+  SkilledList,
+  Text,
+  Footer,
+} from './app';
+
+function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div>
+      <Nav>
+        <Logo href='/'>
+          <svg
+            stroke='currentColor'
+            fill='none'
+            viewBox='0 0 300 300'
+            width='100%'
+            height='100%'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <polygon
+              strokeWidth={10}
+              width={'100%'}
+              height={'100%'}
+              points='150,15 258,77 258,202 150,265 42,202 42,77'
+            />
+            <text
+              x='150'
+              y='150'
+              dominant-baseline='middle'
+              text-anchor='middle'
+              font-size='100'
+              stroke='currentColor'
+              fill='currentColor'
+            >
+              M
+            </text>
+          </svg>
+        </Logo>
+
+        <div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            transition={{ duration: 2.5 }}
+            whileInView={{ opacity: 1 }}
+          >
+            <DesktopList>
+              <DesktopListItem>
+                <a>About</a>
+              </DesktopListItem>
+              <DesktopListItem>
+                <a>Work</a>
+              </DesktopListItem>
+              <DesktopListItem>
+                <a>Contact</a>
+              </DesktopListItem>
+              <Button>Resume</Button>
+            </DesktopList>
+          </motion.div>
+
+          <MenuBar className='md:hidden'>
+            {showMenu ? (
+              <CgClose color='#fff' size={24} onClick={handleShowMenu} />
+            ) : (
+              <FaBars color='#fff' size={24} onClick={handleShowMenu} />
+            )}
+          </MenuBar>
+
+          <MobileList
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            style={{ right: showMenu ? '-12px' : '-100%' }}
+          >
+            <li>
+              <a className='text-blue-50'>About</a>
+            </li>
+            <li>
+              <a className='text-blue-50'>Work</a>
+            </li>
+            <li>
+              <a className='text-blue-50'>Contact</a>
+            </li>
+          </MobileList>
+        </div>
+      </Nav>
+
+      <main
+        style={{
+          color: '#fff',
+          position: 'relative',
+          // minHeight: '90vh',
+          width: '100%',
+          margin: '0 auto',
+          filter: showMenu ? 'blur(5px) brightness(0.7)' : '',
+        }}
+      >
+        <Section>
+          <motion.div>
+            <motion.h3
+              initial={{
+                opacity: 0,
+                y: 30,
+              }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.8, duration: 0.5, x: 0 }}
+              style={{ color: '#cdc5c5' }}
+            >
+              <MiniHeader>Hi, my name is</MiniHeader>
+            </motion.h3>
+            <motion.h2
+              initial={{
+                opacity: 0,
+                y: 30,
+              }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.95, duration: 0.5, x: 0 }}
+            >
+              <HeaderTitle>Mujtaba Adamu</HeaderTitle>
+            </motion.h2>
+
+            <motion.div
+              initial={{
+                opacity: 0,
+                y: 30,
+              }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1, duration: 0.5, x: 0 }}
+            >
+              <Text>
+                My area of expertise is using React and React Native to optimise
+                front-end-to-backend communication while creating user
+                interfaces.
+              </Text>
+              <Text>
+                I possess a strong passion for web development, with a keen
+                focus on creating exceptional web experiences. Ever since my
+                early days of working with computers, coding has ignited my
+                passion, eventually leading me to specialize in web development
+                since 2016. I take great pleasure in designing and building
+                visually stunning, user-friendly, and feature-rich websites.
+              </Text>
+            </motion.div>
+          </motion.div>
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 30,
+            }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.3, duration: 0.5, x: 0 }}
+          >
+            <Button style={{ marginTop: '30px', padding: '15px 20px' }}>
+              Talk to me
+            </Button>
+          </motion.div>
+        </Section>
+
+        <RightMenuIcon>
+          <motion.li>
+            <motion.a
+              href='https://linkedin.com/in/mujtaba-adamu'
+              target='_blank'
+            >
+              <LiaLinkedin size={25} />
+            </motion.a>
+          </motion.li>
+          <motion.li>
+            <motion.a href='https://github.com/Adamumujtaba/' target='_blank'>
+              <GrGithub size={20} />
+            </motion.a>
+          </motion.li>
+          <motion.li>
+            <motion.a href='https://twitter.com/Mujtaba__ibb' target='_blank'>
+              <BsTwitterX size={20} />
+            </motion.a>
+          </motion.li>
+          <motion.li>
+            <motion.a
+              href='https://www.facebook.com/mujtaba.adamu.77'
+              target='_blank'
+            >
+              <FaFacebook size={20} />
+            </motion.a>
+          </motion.li>
+        </RightMenuIcon>
+        <LeftEmail>
+          <motion.a>mujtaba@gmail.com</motion.a>
+        </LeftEmail>
+
+        <Cont>
+          <HeaderAbout>About</HeaderAbout>
+          <Text>
+            I am a full-stack web developer with a passion for creating
+            exceptional user experiences. I have a keen interest in web
+            development, with a strong focus on creating visually stunning,
+            user-friendly, and feature-rich websites.
+          </Text>
+          <Text>
+            Here are a few technologies I've been working with recently:
+          </Text>
+          <SkilledList>
+            <li>(Type, Java)Script</li>
+            <li>HTML & CSS</li>
+            <li>Rest APIs</li>
+            <li>ES6+</li>
+            <li>React</li>
+            <li>React Native</li>
+            <li>Node.js</li>
+            <li>Git & Git(hub,lab)</li>
+          </SkilledList>
+        </Cont>
+
+        <Cont>
+          <Footer>
+            <p className='txt'>Design and build by Mujtaba Adamu</p>
+            <div className='icon'>
+              <motion.li>
+                <motion.a
+                  href='https://linkedin.com/in/mujtaba-adamu'
+                  target='_blank'
+                >
+                  <LiaLinkedin size={25} />
+                </motion.a>
+              </motion.li>
+              <motion.li>
+                <motion.a
+                  href='https://github.com/Adamumujtaba/'
+                  target='_blank'
+                >
+                  <GrGithub size={20} />
+                </motion.a>
+              </motion.li>
+              <motion.li>
+                <motion.a
+                  href='https://twitter.com/Mujtaba__ibb'
+                  target='_blank'
+                >
+                  <BsTwitterX size={20} />
+                </motion.a>
+              </motion.li>
+              <motion.li>
+                <motion.a
+                  href='https://www.facebook.com/mujtaba.adamu.77'
+                  target='_blank'
+                >
+                  <FaFacebook size={20} />
+                </motion.a>
+              </motion.li>
+            </div>
+          </Footer>
+        </Cont>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app.ts
@@ -0,0 +1,249 @@
+import { motion } from 'framer-motion';
+import UserImage from './images/mujtaba.jpg';
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  all: unset;
+  padding: 10px 10px;
+  border-radius: 3px;
+  border: 1px solid #3699f0;
+  font-size: small;
+  font-family: var(--font-mono);
+  min-width: 60px;
+  text-align: center;
+  color: #3699f0;
+  cursor: pointer;
+`;
+export const Section = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 8rem;
+  padding-bottom: 10rem;
+  background-image: url(${UserImage});
+  background-repeat: no-repeat;
+  transition: var(--transition);
+  background-position: center right;
+  background-size: contain;
+  background-attachment: fixed;
+  @media screen and (max-width: 760px) {
+    padding: 0px;
+    margin: 5rem auto;
+  }
+`;
+
+export const MiniHeader = styled.h2`
+  font-weight: 400;
+  font-family: var(--font-mono);
+  font-size: 14px;
+  color: #3699f0;
+`;
+export const HeaderTitle = styled.h1`
+  font-family: var(--font-mono);
+  font-size: clamp(40px, 6vw, 80px);
+  color: #d0d0d0d0;
+`;
+export const Logo = styled.a`
+  display: block;
+  height: 42px;
+  width: 42px;
+  color: #3699f0;
+  font-family: var(--font-sans);
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+  height: 80px;
+  position: relative;
+  width: 95%;
+`;
+
+export const DesktopList = styled.ul`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  /* background: red; */
+  animation-duration: 2000ms;
+  opacity: 1;
+  list-style: none;
+  font-size: small;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const MenuBar = styled.li`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+export const DesktopListItem = styled.li`
+  color: #fff;
+  font-family: var(--font-mono);
+`;
+
+export const MobileList = styled(motion.ul)`
+  display: none;
+  li {
+    color: #fff;
+    list-style-type: none;
+    font-family: var(--font-sans);
+  }
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    align-items: center;
+    position: absolute;
+    padding-top: 3rem;
+    top: 70px;
+    width: 60%;
+    height: 100vh;
+    background-color: #070707;
+    /* background-color: red; */
+    right: -12px;
+    z-index: 3;
+    transition: all 0.5s ease-in-out;
+  }
+`;
+export const RightMenuIcon = styled(motion.ul)`
+  position: fixed;
+  display: flex;
+  flex-direction: row;
+  list-style-position: inside;
+  padding: 0px;
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  align-items: start;
+  gap: 5px;
+  box-sizing: border-box;
+  bottom: 0;
+  left: 20px;
+  &::after {
+    content: '';
+    padding: 0.6px;
+    margin: 5px 10px;
+    background: #fff;
+    height: 130px;
+    width: 0.5px;
+    display: block;
+  }
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+export const LeftEmail = styled(motion.a)`
+  /* background: #0dffce; */
+  /* background: #1546a3; */
+  writing-mode: vertical-lr;
+  position: fixed;
+  display: flex;
+  flex-direction: row;
+  right: 20px;
+  /* left: 0px; */
+  bottom: 0;
+  &:hover {
+    color: #1546a3;
+  }
+  a {
+    font-family: var(--font-mono);
+  }
+  &::after {
+    content: '';
+    padding: 0.6px;
+    margin: 5px 10px;
+    background: #fff;
+    height: 130px;
+    width: 0.4px;
+    display: block;
+  }
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const Text = styled.div`
+  font-family: var(--font-sans);
+  line-height: 1.4rem;
+  text-align: justify;
+  color: #d0d0d0d0;
+`;
+
+export const Cont = styled.div`
+  width: 80%;
+  margin: auto;
+`;
+export const HeaderAbout = styled.h1`
+  font-family: var(--font-mono);
+  font-size: clamp(20px, 4vw, 40px);
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: #d0d0d0d0;
+  &::after {
+    content: '';
+    padding: 1px;
+    width: 300px;
+    background: #d0d0d0d0;
+  }
+`;
+
+export const SkilledList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(140px, 200px));
+  padding: 0px;
+
+  margin: 20px 0px 0px;
+  overflow: hidden;
+  list-style: none;
+  font-family: var(--font-mono);
+  font-size: small;
+  color: #d0d0d0d0;
+  li::before {
+    content: '▹';
+    color: #d0d0d0d0;
+    margin: 10px;
+    line-height: 25px;
+  }
+`;
+// content: "▹";
+
+export const Footer = styled.footer`
+  font-size: smaller;
+  font-family: var(--font-mono);
+  text-align: center;
+  padding: 2rem 0px;
+  color: #d0d0d0d0;
+
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+    gap: 20px;
+    a {
+      color: #d0d0d0d0;
+    }
+  }
+  .txt {
+    display: none;
+  }
+
+  @media screen and (min-width: 768px) {
+    .txt {
+      display: block;
+    }
+    .icon {
+      display: none;
+    }
+  }
+`;
